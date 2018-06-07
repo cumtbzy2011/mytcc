5723aae3025e40c08162d3edf27d9422101a39d9
bzy <cumtbzy2011@163.com>
2018-06-07 10:01:58 +0800

    init

--- user/src/main/java/com/bian/user/TestController.java
+++ user/src/main/java/com/bian/user/TestController.java
@@ -8,6 +8,9 @@ import com.netflix.discovery.shared.Applications;
 import com.netflix.loadbalancer.ILoadBalancer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.netflix.eureka.CloudEurekaClient;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,6 +43,12 @@ public class TestController {
 
     @RequestMapping("usertest")
     public Object test() {
+        RedisTemplate redisTemplate = null;
+        redisTemplate.watch("local");
+        redisTemplate.multi();
+        redisTemplate.exec();
+
+
         EurekaClient eurekaClient = provider.get();
         Applications applications = eurekaClient.getApplications();
         List<Application> registeredApplications = applications.getRegisteredApplications();
@@ -75,4 +84,47 @@ public class TestController {
 
         return "success";
     }
+
+
+    @Autowired
+    RedisTemplate<String, String> redisTemplate;
+
+    public Object setRedis() {
+        BoundValueOperations<String, String> testKey = redisTemplate.boundValueOps("testKey");
+        testKey.set("test1");
+        return "success";
+    }
+
+    public Object getRedis() {
+        BoundValueOperations<String, String> testKey = redisTemplate.boundValueOps("testKey");
+        String result = testKey.get();
+        return result;
+    }
+
+    public Object testWatch1() {
+        List<Object> execute = redisTemplate.execute(new SessionCallback<List<Object>>() {
+
+            @Override
+            public List<Object> execute(RedisOperations redisOperations) throws DataAccessException {
+                redisOperations.multi();
+                redisOperations.opsForValue().set("testKey2", "test2");
+                return redisOperations.exec();
+            }
+        });
+
+        System.out.println(execute);
+        return execute;
+    }
+
+    public List<Object> testPipe() {
+        return redisTemplate.executePipelined(new RedisCallback<Object>() {
+            @Override
+            public Object doInRedis(RedisConnection redisConnection) throws DataAccessException {
+                redisConnection.set("key".getBytes(), "value".getBytes());
+                return null;
+            }
+        });
+    }
+
+
 }
